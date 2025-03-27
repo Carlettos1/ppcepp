58508fb6aee9971677764d799ea2c11bdc1885a5
Carlettos <kako3000@gmail.com>
2025-03-27 12:50:46 -0300

    db reconnect

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,37 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+let db;
+
+const dbConfig = {
     host: "127.0.0.1",
     user: "admin",
     password: "1234",
     database: "login_system",
-});
+};
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL database.');
-});
+function connect() {
+    db = mysql.createConnection(dbConfig);
+
+    db.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MySQL database:', err);
+            setTimeout(connect, 2000); // Retry connection after 2 seconds
+        } else {
+            console.log('Connected to MySQL database.');
+        }
+    });
+
+    db.on('error', (err) => {
+        console.error('MySQL error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
+            console.error('Reconnecting to MySQL database...');
+            connect(); // Reconnect if connection is lost
+        } else {
+            throw err; // Handle other errors
+        }
+    });
+}
+
+connect();
 
 module.exports = db;
