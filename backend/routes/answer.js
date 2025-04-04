9c247b9cb41cfbdcc46e7a8f3f087687679244ce
Carlettos <kako3000@gmail.com>
2025-04-04 19:39:09 -0300

    added grades

--- backend/routes/answer.js
+++ backend/routes/answer.js
@@ -5,6 +5,7 @@ const db = require('../config/db');
     question_id: int,
     user_id: int,
     answer: string
+    grade: float
 */
 
 const router = express.Router();
@@ -20,7 +21,7 @@ router.get('/all', (req, res) => {
 });
 
 router.get('/all/named', (req, res) => {
-    db.query('SELECT answer.id, users.username as user_name, question.title as question, answer.answer as answer FROM answer INNER JOIN users ON answer.user_id = users.id INNER JOIN question ON answer.question_id = question.id ORDER BY users.id DESC', (err, results) => {
+    db.query('SELECT answer.id, users.username as user_name, question.title as question, answer.answer as answer, answer.grade as grade FROM answer INNER JOIN users ON answer.user_id = users.id INNER JOIN question ON answer.question_id = question.id ORDER BY users.id DESC', (err, results) => {
         if (err) {
             return res.status(500).json({ error: 'Internal server error' });
         }
@@ -81,6 +82,29 @@ router.delete('/:id', (req, res) => {
             return res.status(500).json({ error: 'Internal server error' });
         }
         res.json({ message: 'Answer deleted' });
+    });
+});
+
+// update grade
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { grade } = req.body;
+    db.query('UPDATE answer SET grade = ? WHERE id = ?', [grade, id], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json({ message: 'Grade updated' });
+    });
+});
+
+router.get("/teacher/:id", (req, res) => {
+    const { id } = req.params;
+    //db.query("SELECT answer.id AS answer_id, answer.question_id, answer.user_id, users.username, answer.answer, answer.grade FROM answer JOIN users ON answer.user_id = users.id WHERE users.teacher = ?", [id], (err, results) => {
+    db.query("SELECT answer.id, users.username as user_name, question.title as question, answer.answer as answer, answer.grade as grade FROM answer INNER JOIN users ON answer.user_id = users.id INNER JOIN question ON answer.question_id = question.id WHERE users.teacher = ? ORDER BY users.id DESC", [id], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json(results);
     });
 });
 
